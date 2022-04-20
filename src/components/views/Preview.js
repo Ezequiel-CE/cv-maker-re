915efdc48ef935583152ab26c41e73c2e32d8a72
Ezequiel carrizo <ezequielce490@gmail.com>
2022-04-20 16:29:42 -0300

    added preview component

---
+++ src/components/views/Preview.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "@mui/material/Container";
+
+const Preview = () => {
+  return (
+    <Container
+      sx={{
+        bgcolor: "#EEEEEE",
+        padding: "50px",
+        marginTop: "50px",
+        marginBottom: "50px",
+        borderRadius: "10px",
+      }}
+    ></Container>
+  );
+};
+
+export default Preview;
