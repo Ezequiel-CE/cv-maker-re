714b930737408595f4d226dcf0eff72afe38edfd
Ezequiel carrizo <ezequielce490@gmail.com>
2022-04-21 17:24:04 -0300

    adde pdf viewer component

--- src/components/views/PreviewView/Preview.js
+++ src/components/views/PreviewView/Preview.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Container from "@mui/material/Container";
+import { PDFViewer } from "@react-pdf/renderer";
+import MyDocument from "./MyDocument";
 
 const Preview = () => {
   return (
@@ -10,8 +12,19 @@ const Preview = () => {
         marginTop: "50px",
         marginBottom: "50px",
         borderRadius: "10px",
+        height: "100vh",
       }}
-    ></Container>
+    >
+      <PDFViewer
+        showToolbar={false}
+        style={{
+          width: "100%",
+          height: "95%",
+        }}
+      >
+        <MyDocument />
+      </PDFViewer>
+    </Container>
   );
 };
 
